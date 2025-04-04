da6b84d93caba423eae8509d31897ed0f3589f10
iusmankh1 <Usman.911@outlook.com>
2025-04-04 11:15:50 +0500

    Initial commit on dev branch

--- src/navigation/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as DrawerNavigator } from './DrawerNavigator';
-export { default as HomeStack } from './StackNavigator';
-export { default as SettingsStack } from './SettingsStackNavigator'; 
